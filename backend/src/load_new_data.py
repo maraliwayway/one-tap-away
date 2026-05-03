3f5249ddc163aabcc1319199ec227f7b29384c5b
Andy Wang <andywang@u172-016-110-124.wireless.sfu.ca>
2026-05-03 14:35:32 -0700

    swapped from pipenv to uv, changed python version to 3.11

--- backend/src/load_new_data.py
+++ backend/src/load_new_data.py
@@ -1,22 +1,26 @@
 import pandas as pd
+import os
+
 
 SHEET_ID = "1hMsYgDQj3ymqwxUXA7R-ITITnw3HzeVZBxaXAjiJwAE"
 TARGET_GID = 0
 
-import os
-OUTPUT_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'artifacts', 'spreadsheet_data.json')
+OUTPUT_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), "artifacts", "spreadsheet_data.json"
+)
 
 
 def _load_sheet():
     csv_url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={TARGET_GID}"
     return pd.read_csv(csv_url)
 
+
 def _save_to_json(df):
     df.to_json(OUTPUT_PATH, orient="records", indent=2)
+
 
 def load_new_data():
     print("Loading New Data...")
 
     df = _load_sheet()
     _save_to_json(df)
-
